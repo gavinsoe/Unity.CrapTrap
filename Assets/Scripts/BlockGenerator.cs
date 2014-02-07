b5b1fbcdfd3826ee72a1944fb83de89e30e7d9aa
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-02-07 17:11:32 +1100

    Added the character into a prefab Created a purely random level generator

---
+++ Assets/Scripts/BlockGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockGenerator : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public GameObject[] blocks;
+
+	// Use this for initialization
+	void Start () {
+		// Create the blocks.
+		for (int i = 1; i <= width; i++){
+			for (int j = 1; j <= height; j++){
+				transform.position = new Vector3(i,j,0);
+				Instantiate (Randomizer(), transform.position, transform.rotation);
+			}
+		}
+	}
+	
+	GameObject Randomizer(){
+		GameObject block = blocks[0];
+		block.GetComponent<BlockController>().pulledOut = (Random.value > 0.5f);
+		return block;
+	}
+	
+	
+}
