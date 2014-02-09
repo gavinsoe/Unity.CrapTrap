978ce100457d94148af7eda4dc8c921a12a5975c
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-02-09 19:52:28 +1100

    Stage generator now detects character location and creates/remove stage accordingly.

--- Assets/Scripts/LevelChunkFactory.cs
+++ Assets/Scripts/LevelChunkFactory.cs
@@ -4,26 +4,32 @@ using System.Collections;
 public class LevelChunkFactory : MonoBehaviour {
 
 	public GameObject[] levelChunks;
-
+	private bool triggered = false; // A flag to make sure that the generating of levels is only triggered once.
+	
 	void OnTriggerEnter2D(Collider2D col){
 		// Check if player goes past this collider
-		if (col.gameObject.tag == "Player"){
-			// Get location of current chunk
-			Vector3 cur_location = transform.parent.position;
-			
+		
+		if (!triggered && col.gameObject.tag == "Player"){
 			// Get location of the next generated chunk
 			// Retrieve height of the levelChunk
-			var height = transform.parent.GetComponentInChildren<BlockGenerator>().height;
+			var height = transform.GetComponentInChildren<BlockGenerator>().height;
 			
 			// Generate the next section of the stage
-			transform.position = new Vector3(cur_location.x,cur_location.y + height,0);
+			Vector3 next_location = new Vector3(transform.position.x,transform.position.y + height,0);
 			
-			var block = Instantiate (GenerateLevelChunk(), transform.position, transform.rotation) as GameObject;
+			var block = Instantiate (GenerateLevelChunk(), next_location, transform.rotation) as GameObject;
+			triggered = true;
+		} else if ( col.gameObject.tag == "Collider" ){
+			RemoveThisChunk();
 		}
 	}
 	
 	GameObject GenerateLevelChunk(){
 		GameObject chunk = levelChunks[0];
 		return chunk;
+	}
+	
+	void RemoveThisChunk(){
+		Destroy(gameObject);
 	}
 }
