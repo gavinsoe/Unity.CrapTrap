635a58929e1dfa35e49b659e4fbfad3973e42c42
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-02-10 17:24:38 +1100

    Added the following changes for the stage generator - Bottom most layer will consist of all pulled out blocks - Blocks where character spawn will always be pulled in - Walls are removed

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -3,24 +3,52 @@ using System.Collections;
 
 public class BlockGenerator : MonoBehaviour {
 
-	public int width;
-	public int height;
+	public int width, i;
+	public int height, j;
 	public GameObject[] blocks;
 
 	// Use this for initialization
 	void Start () {
-		// Create the blocks.
-		for (int i = 1; i <= width; i++){
-			for (int j = -3; j <= (height-3); j++){
-				transform.position = new Vector3(i,j,0);
-				var block = Instantiate (GenerateBlock(), transform.position, transform.rotation) as GameObject;
-				block.transform.parent = this.transform.parent;
-				block.transform.localPosition = transform.position;
+		if (transform.parent.name == "Bottom Floor"){
+			// Create the blocks (bottom 2 levels)
+			for (i = 1; i <= width; i++){
+				for (j = 1; j <= height; j++){
+					
+					GameObject block = blocks[0]; // Retrieve the normal block
+					if ( j == 1 ){
+						// Set all blocks to be 'pulled out' when it is at the bottom most level.
+						block.GetComponent<BlockController>().pulledOut = true;
+					} else {
+						// Randomize the rest of the blocks
+						block.GetComponent<BlockController>().pulledOut = (Random.value > 0.5f);
+					}
+					
+					transform.position = new Vector3(i,j,0);
+					var new_block = Instantiate (block, transform.position, transform.rotation) as GameObject;
+					new_block.transform.parent = this.transform.parent;
+					new_block.transform.localPosition = transform.position;
+					
+					// checks if block collides with the character, if it does, make sure block is pulled in
+					if (Physics2D.OverlapPoint(new_block.transform.position, 1 << LayerMask.NameToLayer("Character"))){
+						block.GetComponent<BlockController>().pulledOut = false;
+					}
+					
+				}
+			}
+		} else {
+			// Create the blocks.
+			for (i = 1; i <= width; i++){
+				for (j = -3; j <= (height-3); j++){
+					transform.position = new Vector3(i,j,0);
+					var new_block = Instantiate (GenerateRandomBlock(), transform.position, transform.rotation) as GameObject;
+					new_block.transform.parent = this.transform.parent;
+					new_block.transform.localPosition = transform.position;
+				}
 			}
 		}
 	}
 	
-	GameObject GenerateBlock(){
+	GameObject GenerateRandomBlock(){
 		GameObject block = blocks[0];
 		block.GetComponent<BlockController>().pulledOut = (Random.value > 0.5f);
 		return block;
