3a4bf63556b4c431e24940c7a4d42afa76411c92
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-02-11 23:36:31 +1100

    Created a simple parallax background (lots of improvements needed in the future)

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	private float X;
+	public int offset;
+	public bool FollowCamera;
+
+	// Use this for initialization
+	void Start () {
+		X = Camera.main.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FollowCamera){
+			Vector3 camera_pos = Camera.main.transform.position;
+			transform.position = new Vector3((camera_pos.x - X)/offset, camera_pos.y, transform.position.z);
+		} else {
+			Vector3 camera_pos = Camera.main.transform.position;
+			transform.position = new Vector3((X - camera_pos.x)/offset, camera_pos.y, transform.position.z);
+		}
+	}
+}
