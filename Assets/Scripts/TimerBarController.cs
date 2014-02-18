23823d443b5f2046e15f2592f95495b157b52d29
Bayoe Otto <bayoe.otto@kairosrenatus.com>
2014-02-18 18:45:06 +1100

    added a timer bar

---
+++ Assets/Scripts/TimerBarController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerBarController : MonoBehaviour {
+
+	public float maxTime;
+	private float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+		timeElapsed = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeElapsed >= 0) {
+			timeElapsed -= Time.deltaTime;
+			transform.localScale = new Vector3 (timeElapsed / maxTime, 0.5f, 1f);
+		}
+	}
+}
