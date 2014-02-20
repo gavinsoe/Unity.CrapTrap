78d750cb03c643088157d5d99309af7f8d0a88fc
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-02-20 23:13:15 +1100

    Added a sprite randomizer script.  Currently used for normal blocks, but can be used for other things.

---
+++ Assets/Scripts/SpriteRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+		//var new_sprite = sprites[Random.Range(0, sprites.Length)];
+		GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)]; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
