81ad23c8cacf8efe2935cd34c5cdbfbb655731e2
Bayoe Otto <bayoe.otto@kairosrenatus.com>
2014-02-21 14:57:08 +1100

    Implemented Main Menu, and Pause Menu for stage B.1

---
+++ Assets/Scripts/PauseGUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		
+		// Calculate the menu rect
+		Rect r = new Rect (Screen.width * (1f - 0.3f) / 2,
+		              Screen.height * (1f - 0.3f) / 2,
+		              Screen.width * 0.3f,
+		              Screen.height * 0.3f);
+		
+		// Draw menu
+		GUILayout.BeginArea (r);
+		GUILayout.BeginVertical ("box");
+		
+		GUILayout.Label ("Game Paused");
+		
+		// Make the buttons.
+		if(GUILayout.Button("Resume Game")) {
+			enabled = false;
+			Time.timeScale = 1f;
+		}
+		if(GUILayout.Button ("Quit to Main Menu")) {
+			Application.LoadLevel("test");
+		}
+		
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+	}
+}
