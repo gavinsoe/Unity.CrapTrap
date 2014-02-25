2bc361c1188c5ebada2a195091b01ac8f096b5ab
Bayoe Otto <bayoe.otto@kairosrenatus.com>
2014-02-25 14:22:48 +1100

    Fixed Destination

--- Assets/Scripts/DestinationController.cs
+++ Assets/Scripts/DestinationController.cs
@@ -10,7 +10,7 @@ public class DestinationController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Physics2D.OverlapPoint (new Vector2 (transform.position.x, transform.position.y), 1 << LayerMask.NameToLayer("Character"), 9.5f, 10.5f) != null) {
+		if(Physics2D.OverlapPoint (new Vector2 (transform.position.x, transform.position.y), 1 << LayerMask.NameToLayer("Character"), 3.5f, 12.5f) != null) {
 			Time.timeScale = 0;
 			Camera.main.GetComponent<GameCompletedGUI>().enabled = true;
 		}
