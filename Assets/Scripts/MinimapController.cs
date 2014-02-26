c7dff70148ffc3f93191161bd871a53803ff9aad
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-02-26 14:19:44 +1100

    All blocks are now under the 'Terrain' Layer All character components are now under the 'Character' layer Minimap now only shows elements under the 'Terrain'/'Character' Layer Main Camera prefab altered (previously had multiple minimap objects under it)

---
+++ Assets/Scripts/MinimapController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.camera.cullingMask = (1 << LayerMask.NameToLayer("Terrain"))|(1 << LayerMask.NameToLayer("Character"));
+	}
+}
