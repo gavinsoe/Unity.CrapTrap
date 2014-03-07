96a61847aa1507ebacc5bef1b8c5212705608ef2
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-03-07 17:28:29 +1100

    Fixed a bug where character does not detect the destination

--- Assets/Scripts/DestinationController.cs
+++ Assets/Scripts/DestinationController.cs
@@ -2,19 +2,11 @@
 using System.Collections;
 
 public class DestinationController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Collider2D col = Physics2D.OverlapPoint (new Vector2 (transform.position.x, transform.position.y), 1 << LayerMask.NameToLayer("Character"),-0.9f, 0.9f);
-		if( col != null && col.gameObject.name == "Character") {
-			Time.timeScale = 0;
-			Camera.main.GetComponent<GameCompletedGUI>().enabled = true;
-		}
-	}
-
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Character")
+        {
+            col.gameObject.GetComponent<CharacterController>().reachedDestination = true;
+        }
+    }
 }
