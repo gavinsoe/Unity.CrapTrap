082a65b0a44631ca4eb86efd21dda3c35477ec4a
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-04-02 15:46:36 +1100

    Added stage G-1.2 Fixed a minor bug that doesn't detect the destination

--- Assets/Scripts/DestinationController.cs
+++ Assets/Scripts/DestinationController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class DestinationController : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.name == "Character")
+        if (col.gameObject.tag == "Player")
         {
             col.gameObject.GetComponent<CharacterController>().reachedDestination = true;
         }
