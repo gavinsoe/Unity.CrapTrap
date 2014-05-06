8ec3275575abd28fc5cdbb45c1d5560b5077970c
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-05-06 14:53:19 +1000

    Updated the main Game GUI: - Score (or number of toilet tissues obtained) is now updated with new graphics - Minimap has a border around it - Timer is changed into a pulsing border

--- Assets/Scripts/MainGameController.cs
+++ Assets/Scripts/MainGameController.cs
@@ -1,24 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainGameController : MonoBehaviour {
+public class MainGameController : MonoBehaviour
+{
+
+    #region Timer Variables
+    public float maxTime;
+    private float timeElapsed;
+    private float timerReductionRate = 1; // Defaults to 1
+    #endregion
 
     // Components
     private TimerBarController timer; // The game timer
-    private ScoreController score;
     private MainGameGUI main;
 
 	// Use this for initialization
 	void Start () {
+        // Initialise timer components
+        Time.timeScale = 1f;
+        timeElapsed = maxTime;
+
         timer = gameObject.GetComponentInChildren<TimerBarController>();
-        score = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreController>();
         main = gameObject.GetComponentInChildren<MainGameGUI>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    // Update timer
+        if (timeElapsed >= 0)
+        {
+            timeElapsed -= Time.deltaTime * timerReductionRate;
+            updateTimerPulseRate();
+        }
+        else
+        {
+            GameOver();
+        }
 	}
+
+    public void updateTimerPulseRate()
+    {
+        if ((timeElapsed / maxTime) > 0.75)
+        {
+            main.timerPulseRate = 2;
+        }
+        else if ((timeElapsed / maxTime) > 0.5)
+        {
+            main.timerPulseRate = 1f;
+        }
+        else if ((timeElapsed / maxTime) > 0.25)
+        {
+            main.timerPulseRate = 0.75f;
+        }
+        else if ((timeElapsed / maxTime) > 0.15)
+        {
+            main.timerPulseRate = 0.5f;
+        }
+        else if ((timeElapsed / maxTime) > 0.05)
+        {
+            main.timerPulseRate = 0.25f;
+        }
+    }
 
     public void setTimerReductionRate(float rate)
     {        
@@ -54,7 +96,7 @@ public class MainGameController : MonoBehaviour {
 		GameObject.FindGameObjectWithTag("Player").GetComponent<PCControls>().enabled = false;
         GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerTouchControls>().enabled = false;
         // Disable Timer
-		Camera.main.GetComponentInChildren<TimerBarController>().enabled = false;
+		// Camera.main.GetComponentInChildren<TimerBarController>().enabled = false;
 	}
 
 	public void EnableTimeNMove() {
@@ -62,6 +104,6 @@ public class MainGameController : MonoBehaviour {
         GameObject.FindGameObjectWithTag("Player").GetComponent<PCControls>().enabled = true;
         GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerTouchControls>().enabled = true;
         // Enable Timer
-		Camera.main.GetComponentInChildren<TimerBarController>().enabled = true;
+		//Camera.main.GetComponentInChildren<TimerBarController>().enabled = true;
 	}
 }
--- Assets/Scripts/MainGameGUI.cs
+++ Assets/Scripts/MainGameGUI.cs
@@ -1,40 +1,105 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainGameGUI : MonoBehaviour {
-    
-    public int ntp = 0;
-    public int gtp = 0;
+public class MainGameGUI : MonoBehaviour
+{
+    #region Currency boxes variables
+    public int ntp = 0; // Keeps track of the number of normal toilet papers collected
+    public int gtp = 0; // Keeps track of the number of golden toilet papers collected
 
-    public Texture ntpTexture; // Texture for 'normal toilet paper'
-    public Texture gtpTexture; // Texture for 'golden toilet paper'
-    public GUIStyle gtpStyle;
-    public GUIStyle ntpStyle;
+    public GUIStyle gtpStyle; // The style for the 'golden toilet paper' currency
+    public GUIStyle ntpStyle; // The style for the 'normal toilet paper' currency
 
-    public float fontScale = 0.04f;
-    public float xOffset = 0.03f;
-    public float yOffset = -0.055f;
+    private float currencyFontScale = 0.04f; // Variable used to scale font for different window sizes
+    private float currencyXOffset = 0.03f; // Variable used to control the x offset of the text in the box
+    private float currencyYOffset = -0.055f; // Variable used to control the y offset of the text in the box
+
+    private float currencyBoxHeight; // The calculated height of the currency box
+    private float currencyBoxWidth; // The calculated width of the currency box
+    #endregion
+
+    #region Minimap variables
+    private Camera minimap; // the object holding the minimap
+    private Rect mapCanvas; // the size of the minimap
+
+    public GUIStyle mapCanvasStyle; // Styling for the minimap (border and such)
+    #endregion
+
+    #region Timer border variables
+    private Rect timerBorder; // Timer border (around edges of screen
+    public GUIStyle timerBorderStyle; // The styling for the border
+    public float timerPulseRate; // The rate at which the timer border pulses
+    #endregion
     
-    // Make the box
+    void Start()
+    {
+        // set the timer border
+        timerBorder = new Rect(0, 0, Screen.width, Screen.height);
+
+        // retrieve the minimap
+        minimap = GameObject.FindGameObjectWithTag("Minimap").GetComponent<Camera>();
+        mapCanvas = minimap.pixelRect;
+        mapCanvas.y = Screen.height - mapCanvas.y - mapCanvas.height;
+
+        // Calculate currency box dimensions.
+        currencyBoxHeight = Screen.height / 12;
+        currencyBoxWidth = Screen.width / 8;
+
+        ntpStyle.fontSize = (int)(Screen.height * currencyFontScale);
+        ntpStyle.contentOffset = new Vector2(currencyBoxWidth * currencyXOffset, currencyBoxHeight * currencyYOffset);
+
+        gtpStyle.fontSize = (int)(Screen.height * currencyFontScale);
+        gtpStyle.contentOffset = new Vector2(currencyBoxWidth * currencyXOffset, currencyBoxHeight * currencyYOffset);
+    }
+
+    // Draw the GUI
     void OnGUI(){
+        // Draw the timer border
+        TimerPulseBorder(timerPulseRate);
 
-        float height = Screen.height / 14;
-        float width = Screen.width / 8;
-        NTPScore(width, height);
-        GTPScore(width, height);
+        // Draw the minimap border
+        MinimapBorder();
+
+        // Draw the currency boxes.
+        NTPScore(currencyBoxWidth, currencyBoxHeight);
+        GTPScore(currencyBoxWidth, currencyBoxHeight);
+    }
+
+    void TimerPulseBorder(float pulse_rate)
+    {
+        float half_pulse_rate = pulse_rate / 2;
+        Color color_alpha1 = new Color(GUI.color.r, GUI.color.g, GUI.color.b, 1);
+        Color color_alpha0 = new Color(GUI.color.r, GUI.color.g, GUI.color.b, 0);
+
+        float timeState = Time.time % pulse_rate;
+        if (timeState % pulse_rate < half_pulse_rate)
+        {
+            GUI.color = Color.Lerp(color_alpha1, color_alpha0, timeState / half_pulse_rate);
+            GUI.Box(timerBorder, "", timerBorderStyle);
+            //Set the color back for other GUI elements
+            GUI.color = color_alpha1;
+        }
+        else
+        {
+            GUI.color = Color.Lerp(color_alpha0, color_alpha1, (timeState - half_pulse_rate) / half_pulse_rate);
+            GUI.Box(timerBorder, "", timerBorderStyle);
+            //Set the color back for other GUI elements
+            GUI.color = color_alpha1;
+        }
+    }
+
+    void MinimapBorder()
+    {
+        GUI.Box(mapCanvas, "", mapCanvasStyle);
     }
 
     void NTPScore(float width, float height)
     {
-        ntpStyle.fontSize = (int)(Screen.height * fontScale);
-        ntpStyle.contentOffset = new Vector2(width * xOffset, height * yOffset);
-        GUI.Label(new Rect(Screen.width - 2 * width, 0, width, height), ntp.ToString() , ntpStyle);
+        GUI.Label(new Rect(Screen.width - 2 * width, height/4, width, height), ntp.ToString() , ntpStyle);
     }
 
     void GTPScore(float width, float height)
     {
-        gtpStyle.fontSize = (int)(Screen.height * fontScale);
-        gtpStyle.contentOffset = new Vector2(width * xOffset, height * yOffset);
-        GUI.Label(new Rect(Screen.width - width, 0, width, height), gtp.ToString() , gtpStyle);
+        GUI.Label(new Rect(Screen.width - width, height/4, width, height), gtp.ToString(), gtpStyle);
     }
 }
