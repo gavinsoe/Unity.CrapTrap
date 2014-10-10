dadeabe4c9d00ccea2b4a7544e887a62c4b1b482
Bayoe Otto <bayoe.otto@kairosrenatus.com>
2014-10-10 23:21:06 +1100

    changed game.cs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-[XmlRoot("Game")]
 public class Game : MonoBehaviour {
 
-    [XmlArray("Achievements")]
+    public static Game game;
+
     public Achievement[] achievements;
     public Dictionary<Type2, double> stats;
 
@@ -45,6 +46,19 @@ public class Game : MonoBehaviour {
 	void Start () {
 	    
 	}
+
+    void Awake()
+    {
+        if (game == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            game = this;
+        }
+        else if (game != this)
+        {
+            Destroy(gameObject);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -123,33 +137,101 @@ public class Game : MonoBehaviour {
 	// function to save all the data
     public void Save()
     {
-        string path = "game.data";
-        var serializer = new XmlSerializer(typeof(Game));
-        using (var stream = new FileStream(path, FileMode.Create))
-        {
-            serializer.Serialize(stream, this);
-        }
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gameInfo.dat");
+
+        GameInfo info = new GameInfo();
+        info.achievements = achievements;
+        info.achievementUnlocked = stats[Type2.achievementUnlocked];
+        info.audio = audio;
+        info.bag = bag;
+        info.bagSlots = bagSlots;
+        info.boughtGearID = stats[Type2.boughtGearID];
+        info.challengeChapterUnlocked = challengeChapterUnlocked;
+        info.challengeLevelsUnlocked = challengeLevelsUnlocked;
+        info.challengeStars = challengeStars;
+        info.chapterUnlocked = chapterUnlocked;
+        info.consecutiveLogins = stats[Type2.consecutiveLogins];
+        info.energy = energy;
+        info.energyCap = energyCap;
+        info.energyFull = energyFull;
+        info.goldenPapers = stats[Type2.goldenPapers];
+        info.isUnlimitedEnergy = isUnlimitedEnergy;
+        info.itemsBought = stats[Type2.itemsBought];
+        info.itemsUsed = stats[Type2.itemsUsed];
+        info.lastLogin = lastLogin.ToString();
+        info.levelsUnlocked = levelsUnlocked;
+        info.objectivesEarned = stats[Type2.objectivesEarned];
+        info.playingTime = stats[Type2.playingTime];
+        info.skillsUsed = stats[Type2.skillsUsed];
+        info.stagesCompleted = stats[Type2.stagesCompleted];
+        info.stagesUnlocked = stats[Type2.stagesUnlocked];
+        info.stars = stars;
+        info.timeSinceFirstEnergy = timeSinceFirstEnergy.ToString();
+        info.toiletPapers = stats[Type2.toiletPapers];
+        info.totalClimbs = stats[Type2.totalClimbs];
+        info.totalHangingSteps = stats[Type2.totalHangingSteps];
+        info.totalPullOuts = stats[Type2.totalPullOuts];
+        info.totalPulls = stats[Type2.totalPulls];
+        info.totalPushes = stats[Type2.totalPushes];
+        info.totalSlides = stats[Type2.totalSlides];
+        info.totalSteps = stats[Type2.totalSteps];
+        info.treasures = stats[Type2.treasures];
+
+        bf.Serialize(file, info);
+        file.Close();
     }
 
 	// function to load the data; if there is no previous saved file then create a new object
-    public static Game Load() {
-        string path = "game.data";
-        var serializer = new XmlSerializer(typeof(Game));
-        if (File.Exists(path))
+    public void Load() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+        GameInfo info;
+        if(File.Exists((Application.persistentDataPath + "/gameInfo.dat"))) 
         {
-            using (var stream = new FileStream(path, FileMode.Open))
-            {
-                return serializer.Deserialize(stream) as Game;
-            }
-        }
-        else
-        {
-            using (var stream = new FileStream(path, FileMode.Create))
-            {
-                Game newG = new Game();
-				newG.Initialize();
-                return newG;
-            }
+            file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Open);
+            info = (GameInfo)bf.Deserialize(file);
+            file.Close();
+
+            achievements = info.achievements;
+            stats[Type2.achievementUnlocked] = info.achievementUnlocked;
+            audio = info.audio;
+            bag = info.bag;
+            bagSlots = info.bagSlots;
+            stats[Type2.boughtGearID] = info.boughtGearID;
+            challengeChapterUnlocked = info.challengeChapterUnlocked;
+            challengeLevelsUnlocked = info.challengeLevelsUnlocked;
+            challengeStars = info.challengeStars;
+            chapterUnlocked = info.chapterUnlocked;
+            stats[Type2.consecutiveLogins] = info.consecutiveLogins;
+            energy = info.energy;
+            energyCap = info.energyCap;
+            energyFull = info.energyFull;
+            stats[Type2.goldenPapers] = info.goldenPapers;
+            isUnlimitedEnergy = info.isUnlimitedEnergy;
+            stats[Type2.itemsBought] = info.itemsBought;
+            stats[Type2.itemsUsed] = info.itemsUsed;
+            lastLogin = System.DateTime.Parse(info.lastLogin);
+            levelsUnlocked = info.levelsUnlocked;
+            stats[Type2.objectivesEarned] = info.objectivesEarned;
+            stats[Type2.playingTime] = info.playingTime;
+            stats[Type2.skillsUsed] = info.skillsUsed;
+            stats[Type2.stagesCompleted] = info.stagesCompleted;
+            stats[Type2.stagesUnlocked] = info.stagesUnlocked;
+            stars = info.stars;
+            timeSinceFirstEnergy = System.DateTime.Parse(info.timeSinceFirstEnergy);
+            stats[Type2.toiletPapers] = info.toiletPapers;
+            stats[Type2.totalClimbs] = info.totalClimbs;
+            stats[Type2.totalHangingSteps] = info.totalHangingSteps;
+            stats[Type2.totalPullOuts] = info.totalPullOuts;
+            stats[Type2.totalPulls] = info.totalPulls;
+            stats[Type2.totalPushes] = info.totalPushes;
+            stats[Type2.totalSlides] = info.totalSlides;
+            stats[Type2.totalSteps] = info.totalSteps;
+            stats[Type2.treasures] = info.treasures;
+
+            energy = checkAndGetEnergy();
+            checkUnlimitedEnergy();
         }
     }
 
@@ -193,6 +275,14 @@ public class Game : MonoBehaviour {
             }
         }
         return energy;
+    }
+
+    public void checkUnlimitedEnergy()
+    {
+        if(System.DateTime.Now.CompareTo(unlimitedEnergyStart.Add(unlimitedEnergySpan)) > 0) 
+        {
+            isUnlimitedEnergy = false;
+        }
     }
 
 	// function to set the last time the user uses the application
@@ -244,4 +334,57 @@ public class Game : MonoBehaviour {
             lockedLevels.Add(key, false);
         }
     } */
+}
+
+[System.Serializable]
+class GameInfo
+{
+    public double totalSteps = 0;
+    public double totalClimbs = 0;
+    public double playingTime = 0;
+    public double toiletPapers = 0;
+    public double goldenPapers = 0;
+    public double totalPulls = 0;
+    public double totalPushes = 0;
+    public double totalPullOuts = 0;
+    public double totalHangingSteps = 0;
+    public double totalSlides = 0;
+    public double treasures = 0;
+    public double stagesCompleted = 0;
+    public double itemsUsed = 0;
+    public double itemsBought = 0;
+    public double stagesUnlocked = 0;
+    public double objectivesEarned = 0;
+    public double boughtGearID = 0;
+    public double skillsUsed = 0;
+    public double achievementUnlocked = 0;
+    public double consecutiveLogins = 0;
+
+    public Achievement[] achievements;
+
+    // chapter unlock variables -- TRUE means unlocked
+    public bool[] chapterUnlocked;
+    public bool[] challengeChapterUnlocked;
+
+    // stars for each stage
+    public int[][] stars;
+    public int[][] challengeStars;
+
+    // stage unlock variables -- TRUE means unlocked
+    public bool[][] levelsUnlocked;
+    public bool[][] challengeLevelsUnlocked;
+
+    public string lastLogin;
+    public string[] bag;
+    public int bagSlots;
+    public bool audio;
+
+    public int energy;
+    public int energyCap;
+    public string timeSinceFirstEnergy;
+    public bool energyFull;
+
+    public bool isUnlimitedEnergy;
+    public string unlimitedEnergySpan;
+    public string unlimitedEnergyStart;
 }
