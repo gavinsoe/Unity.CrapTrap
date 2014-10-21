71869798ca84f3dae5116d5e539427de68740633
Gavin Soebiantoro <gavin@kairosrenatus.com>
2014-10-21 23:18:22 +1100

    Implemented the capsule loot prefab (not tested yet with the stage complete screen)

---
+++ Assets/Scripts/Objects/Capsule.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Capsule : System.Object
+{
+    public Loot[] loot;
+
+    public Loot obtainLoot()
+    {    
+        var index = Random.Range(0.00f, 100.00f);
+        
+        foreach (Loot lootItem in loot)
+        {
+            index = index - lootItem.percentageChance;
+            if (index < 0) return lootItem;
+        }
+
+        // Return first element by default
+        return loot[0];
+    }
+}
---
+++ Assets/Scripts/Objects/Loot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Loot : System.Object
+{
+    public string itemID;
+    public float percentageChance;
+    public int quantity;
+
+    Texture2D icon
+    {
+        get
+        {
+            return (Texture2D)Resources.Load(itemID);
+        }
+    }
+}
